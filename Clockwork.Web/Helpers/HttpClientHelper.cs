7cc20cfbe5d6b773d90a516d731dc0a6653527a5
Jacel Enova <jacel.enova@yahoo.com>
2019-04-03 23:27:32 +0800

    Add Post to ClientHelper

--- Clockwork.Web/Helpers/HttpClientHelper.cs
+++ Clockwork.Web/Helpers/HttpClientHelper.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -33,6 +34,20 @@ namespace Clockwork.Web.Helpers
             {
                 var content = await response.Content.ReadAsStringAsync();
                 result = JsonConvert.DeserializeObject<List<T>>(content);
+            }
+            return result;
+        }
+
+        public static async Task<T> PostRequest<T>(string apiUrl, dynamic postBody)
+        {
+            var result = default(T);
+            var jsonBody = JsonConvert.SerializeObject(postBody);
+
+            var response = await restClient.PostAsync(apiUrl, new StringContent(jsonBody, Encoding.UTF8, "application/json")).ConfigureAwait(false);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<T>(content);
             }
             return result;
         }
