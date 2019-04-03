bc2612062e48f830192a67c66ae3118494b8c7ab
Jacel Enova <jacel.enova@yahoo.com>
2019-04-04 01:46:50 +0800

    Add model to web

---
+++ Clockwork.Web/Models/CurrentTimeQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clockwork.Web.Models
+{
+    public class CurrentTimeQuery
+    {
+        public int CurrentTimeQueryId { get; set; }
+        public DateTime Time { get; set; }
+        public string ClientIp { get; set; }
+        public DateTime UTCTime { get; set; }
+        public string TimeZone { get; set; }
+    }
+
+    public class CurrentTimeQueries
+    {
+        public IEnumerable<CurrentTimeQuery> TimeQueries { get; set; }
+    }
+
+    public class HomeModel
+    {
+        public IEnumerable<CurrentTimeQuery> TimeQueries { get; set; }
+        public IReadOnlyCollection<TimeZoneInfo> TimeZones { get; set; }
+    }
+}
